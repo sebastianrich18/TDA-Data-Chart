b50db51e57ddf1393c3d85fa3ff781acd0aec92b
Sebastian <sebastianrichel18@gmail.com>
2021-04-03 00:23:42 -0400

    v1.0

--- app.js
+++ app.js
@@ -3,20 +3,54 @@ const tda = require('tda-api-client');
 const fs = require('fs');
 
 
+
 let getValue = async () => {
     let accounts = await tda.accounts.getAccounts({ fields: "positions" })
     let totalValue = 0;
     totalValue += await accounts[0]['securitiesAccount']['currentBalances']['liquidationValue'];
     totalValue += await accounts[1]['securitiesAccount']['currentBalances']['liquidationValue'];
-    handleValue(totalValue);
+    writeValue(totalValue);
 }
 
-let handleValue = (accVal) => {
-    console.log(accVal)
-    var writer = csvWriter()
-    writer.pipe(fs.createWriteStream('data/value.csv'), {flags: 'a'});
+let getPositions = async () => {
+    let rawPositions = await tda.accounts.getAccounts({ fields: "positions" })
+    let positions = rawPositions[0]['securitiesAccount']['positions'];
+    positions.concat(rawPositions[0]['securitiesAccount']['positions'])
+    positions.forEach(async p => {
+        await positions.push(p);
+    })
+    writePositions(positions)
+}
+
+let writePositions = (positions) => {
+    let writer = csvWriter({ sendHeaders: false })
+    writer.pipe(fs.createWriteStream('data/positions.csv', { "flags": 'a' }));
+    
+    for (let p of positions) {
+        let obj = {
+            time: new Date().getTime(), 
+            ticker: p['instrument']['symbol'],
+            avgPrice: p['averagePrice'],
+            shares: p['longQuantity'],
+            value: p['marketValue']
+        }
+        writer.write(obj)
+    }
+    writer.end()
+    console.log('DONE WRITING POSITIONS')
+}
+
+let writeValue = (accVal) => {
+    var writer = csvWriter({ sendHeaders: false })
+    writer.pipe(fs.createWriteStream('data/value.csv', { "flags": 'a' }));
     writer.write({ time: new Date().getTime(), value: accVal })
     writer.end()
+    console.log('DONE WRITING VALUES')
 }
 
+
+console.log(new Date());
+console.log("GETTING TD AMERATRADE DATA")
+
 getValue();
+getPositions();
